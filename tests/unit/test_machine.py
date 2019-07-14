4f444e2b42e1daea40d8f0472eeda880881c057a
jg75 <james@amberengine.com>
2019-07-14 13:43:25 -0400

    tweak tests

--- tests/unit/test_machine.py
+++ tests/unit/test_machine.py
@@ -1,12 +1,12 @@
-import random
+from random import choice
 from unittest.mock import create_autospec
 
-import pytest
+from pytest import fixture, raises
 
 from dfsmpy import StateMachine, StopMachine
 
 
-@pytest.fixture(scope="function")
+@fixture(scope="function")
 def simple_machine():
     return {
         "alphabet": {1, 2, 3, 4},
@@ -23,7 +23,6 @@ def test_blueprint(simple_machine):
 
     assert machine.blueprint["context"] == simple_machine.get("context", {})
     assert machine.blueprint["alphabet"] == simple_machine["alphabet"]
-    assert machine.state == simple_machine["initialState"]
 
     for state in simple_machine["validStates"]:
         assert machine.is_valid(state)
@@ -37,27 +36,28 @@ def test_blueprint(simple_machine):
     assert machine.is_initial(machine.state)
     assert not machine.is_accepted(machine.state)
     assert not machine.is_final(machine.state)
+    assert machine.context == machine.blueprint["context"]
     assert not machine.accepted
 
 
 def test_blueprint_invalid_initial_state(simple_machine):
     simple_machine["initialState"] = -1
 
-    with pytest.raises(ValueError):
+    with raises(ValueError):
         StateMachine(simple_machine)
 
 
-def test_transition_function(simple_machine):
+def test_transition_function(simple_machine, event=1):
     mock_transition = create_autospec(simple_machine["transition"])
-    mock_transition.return_value = 1
+    mock_transition.return_value = event
     simple_machine["transition"] = mock_transition
     machine = StateMachine(simple_machine)
 
-    machine.transition(1)
-    mock_transition.assert_called_once_with(machine.context, 1)
+    machine.transition(event)
+    mock_transition.assert_called_once_with(machine.context, event)
 
 
-def test_transition_context(simple_machine):
+def test_transition_context(simple_machine, event=2):
     def transition(context, event):
         context["value"] = event
         return event
@@ -67,66 +67,64 @@ def test_transition_context(simple_machine):
 
     assert machine.context == simple_machine.get("context", {})
 
-    machine.transition(2)
+    machine.transition(event)
 
-    assert machine.state == 2
-    assert machine.context["value"] == 2
-    assert not machine.is_initial(machine.state)
-    assert not machine.is_accepted(machine.state)
-    assert not machine.is_final(machine.state)
+    assert machine.state == event
     assert not machine.accepted
+    assert machine.context["value"] == event
 
 
-def test_transition_accepted(simple_machine):
+def test_transition_accepted(simple_machine, event=3):
     machine = StateMachine(simple_machine)
 
-    machine.transition(3)
-    assert machine.state == 3
-    assert not machine.is_initial(machine.state)
-    assert machine.is_accepted(machine.state)
-    assert not machine.is_final(machine.state)
+    machine.transition(event)
+    assert machine.state == event
     assert machine.accepted
 
 
-def test_transition_final_state(simple_machine):
+def test_transition_final_state(simple_machine, event=4):
     machine = StateMachine(simple_machine)
 
-    machine.transition(4)
-    assert machine.state == 4
-    assert not machine.is_initial(machine.state)
-    assert not machine.is_accepted(machine.state)
-    assert machine.is_final(machine.state)
+    machine.transition(event)
+    assert machine.state == event
+    assert not machine.accepted
 
-    with pytest.raises(StopMachine):
-        machine.transition(1)
+    with raises(StopMachine):
+        machine.transition(event)
 
 
 def test_transition_invalid_event(simple_machine):
     machine = StateMachine(simple_machine)
 
-    with pytest.raises(ValueError):
+    with raises(ValueError):
         machine.transition(-1)
 
 
-def test_transition_invalid_state(simple_machine):
+def test_transition_invalid_state(simple_machine, event=1):
     simple_machine["transition"] = lambda *_: -1
     machine = StateMachine(simple_machine)
 
-    with pytest.raises(ValueError):
-        machine.transition(1)
+    with raises(ValueError):
+        machine.transition(event)
 
 
-def test_reset(simple_machine):
+def test_reset(simple_machine, event=3):
+    def transition(context, event):
+        context["value"] = event
+        return event
+
+    simple_machine["transition"] = transition
     machine = StateMachine(simple_machine)
-    machine.context["value"] = "value"
 
-    machine.transition(3)
+    machine.transition(event)
+    assert machine.context == {"value": event}
+
     machine.reset()
     assert machine.is_initial(machine.state)
     assert machine.context == machine.blueprint["context"]
 
 
-@pytest.fixture(scope="session")
+@fixture(scope="session")
 def binary_multiples():
     """
     Return a state machine blueprint that accepts binary numbers that are
@@ -163,7 +161,7 @@ def test_machine(binary_multiples):
         while not machine.is_accepted(machine.state) \
                 or not result \
                 or result in binary_multiples["context"]["results"]:
-            event = random.choice(list(machine.blueprint["alphabet"]))
+            event = choice(list(machine.blueprint["alphabet"]))
 
             machine.transition(event)
 
